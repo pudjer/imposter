860bc71c43b81fa5a9ae91e38c867f0f34083d72
tirettur <tirettur@gmail.com>
2026-04-14 20:32:37 +0300

    фывафыва

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1,32 +1,12 @@
 export const POPULAR_CHARACTERS: string[]  = [
-  // === 20 Героев Dota 2 (самые иконные) ===
-  "Pudge", "Invoker", "Phantom Assassin", "Crystal Maiden", "Juggernaut",
-  "Axe", "Sniper", "Rubick", "Earthshaker", "Techies",
-  "Shadow Fiend", "Anti-Mage", "Queen of Pain", "Storm Spirit", "Mirana",
-  "Slark", "Phantom Lancer", "Tinker", "Lina", "Zeus",
-
-  // === 20 Аниме персонажей (иконы поп-культуры) ===
-  "Goku", "Naruto", "Luffy", "Levi Ackerman", "Eren Yeager",
-  "Sailor Moon", "Spike Spiegel", "Lelouch Lamperouge", "Light Yagami", "Edward Elric",
-  "Saitama", "Gojo Satoru", "Tanjiro Kamado", "Jotaro Kujo", "Guts",
-  "L", "Mikasa Ackerman", "Vegeta", "Kakashi Hatake", "Nezuko Kamado",
-
-  // === 20 Персонажей мультфильмов (Disney, Pixar, WB) ===
-  "Mickey Mouse", "Bugs Bunny", "SpongeBob SquarePants", "Shrek", "Homer Simpson",
-  "Rick Sanchez", "Eric Cartman", "Simba", "Elsa", "Woody",
-  "Scooby-Doo", "Tom and Jerry", "Pikachu", "Gollum", "Frodo Baggins",
-  "Peter Griffin", "Kenny McCormick", "Winnie the Pooh", "Lightning McQueen", "Gru",
-
-  // === 20 Исторических личностей (узнаваемые иконы) ===
-  "Albert Einstein", "Leonardo da Vinci", "Cleopatra", "Julius Caesar", "Napoleon Bonaparte",
-  "Jesus Christ", "Muhammad Ali", "William Shakespeare", "Abraham Lincoln", "Winston Churchill",
-  "Adolf Hitler", "Genghis Khan", "Marie Curie", "Isaac Newton", "George Washington",
-  "Martin Luther King Jr.", "Nelson Mandela", "Socrates", "Charles Darwin", "Joan of Arc",
-
-  // === 20 Героев Марвел (MCU + комиксы) ===
-  "Spider-Man", "Iron Man", "Captain America", "Thor", "Hulk",
-  "Black Widow", "Wolverine", "Deadpool", "Doctor Strange", "Black Panther",
-  "Thanos", "Loki", "Scarlet Witch", "Vision", "Star-Lord",
-  "Groot", "Rocket Raccoon", "Daredevil", "Magneto", "Venom"
+  "Anti-Mage", "Axe", "Bane", "Bloodseeker", "Crystal Maiden",
+  "Drow Ranger", "Earthshaker", "Juggernaut", "Lina", "Lion",
+  "Mirana", "Monkey King", "Necrophos", "Pudge", "Phantom Assassin",
+  "Riki", "Shadow Fiend", "Sniper", "Techies", "Tinker",
+  "Ursa", "Venomancer", "Viper", "Windranger", "Zeus",
+  "Alchemist", "Beastmaster", "Brewmaster", "Centaur Warrunner", "Chaos Knight",
+  "Clinkz", "Clockwerk", "Death Prophet", "Disruptor", "Doom",
+  "Dragon Knight", "Ember Spirit", "Enigma", "Faceless Void", "Gyrocopter",
+  "Huskar", "Invoker", "Io", "Jakiro", "Kunkka",
+  "Legion Commander", "Lifestealer", "Luna", "Magnus", "Medusa"
 ];
-
